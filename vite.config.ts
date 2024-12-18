532e6f3f0a778ea9062f6cf6a0bdeb17ed3106a4
Armen Kirakosyan <armen.kirakosyan@goya.am>
2024-12-18 05:26:59 +0400

    Backed up base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
-  base: '/mv-tech-task/',
+  plugins: [TanStackRouterVite({}), react()]
 })
